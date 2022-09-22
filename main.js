ce48bfa61205fb544f83fbc2faf1693f3fb5633d
KleitonAS00 <kleitonsilva.24@hotmail.com>
2022-09-22 18:46:42 -0300

    corrigi exercicio diminuindo a quantidade de codg.

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
-        const novaTarefa = $('#nomeTarefa').val();
+
+        const novaTarefa = $("#nomeTarefa").val();
+
+        $(`<li>${novaTarefa}</li>`).appendTo("ul");
+
+        $("#nomeTarefa").val("");
+
+        $('ul li').click(function () {
+            $(this).addClass('riscar btn');
+        });
+
+        /*const novaTarefa = $('#nomeTarefa').val();
         const novaLinha = $('<li></li>');
         $(`${novaTarefa}`).appendTo(novaLinha);
         $(`
-            <li><button class="btn" type="button">${novaTarefa}</button></li>
+            <button class="btn" type="button">${novaTarefa}</button>
             `).appendTo(novaLinha);
             $(novaLinha).appendTo('ul');
-            $('#nomeTarefa').val('');
-
-            $('ul li button').click(function () {
-                $(this).addClass('riscar btn');
-            });    
+            $('#nomeTarefa').val('');*/
     });
 })
 
